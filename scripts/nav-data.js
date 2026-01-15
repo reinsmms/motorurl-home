398679081d4123c5391aabc36ba927209db23d37
reinsmms <matthewreinsmith@outlook.com>
2026-01-14 21:25:06 -0500

    20260114_Updates_V2.zip

--- scripts/nav-data.js
+++ scripts/nav-data.js
@@ -28,36 +28,9 @@ window.MOTORURL_NAV = [
         "showGalleryLink": false
       },
       {
-        "title": "Page 1",
-        "href": "pages/page1.html",
-        "galleryPath": "assets/example-subject",
-        "showGalleryLink": true
-      },
-      {
-        "title": "Page 2",
-        "href": "pages/page2.html",
-        "showGalleryLink": false
-      },
-      {
         "title": "Material Resistance - Quick Reference",
         "href": "pages/material-resistance-quick-reference.html",
         "showGalleryLink": false
-      }
-    ]
-  },
-  {
-    "id": "set2",
-    "title": "Set 2",
-    "openByDefault": true,
-    "href": "pages/set-2.html",
-    "items": [
-      {
-        "title": "Page 3",
-        "href": "pages/page3.html"
-      },
-      {
-        "title": "Page 4",
-        "href": "pages/page4.html"
       }
     ]
   }
